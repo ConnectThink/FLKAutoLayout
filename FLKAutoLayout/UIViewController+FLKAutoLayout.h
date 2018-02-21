7df1215a6ecde4ed7cfc88816c2eaa447c936432
Matt Lathrop <mslathrop@gmail.com>
2018-02-21 14:46:20 -0500

    Add support for safeAreaLayoutGuide

--- FLKAutoLayout/UIViewController+FLKAutoLayout.h
+++ FLKAutoLayout/UIViewController+FLKAutoLayout.h
@@ -1,17 +1,17 @@
 #import <UIKit/UIKit.h>
 
-/// A NSLayoutGuide for iOS 7 & 8
+/// A NSLayoutGuide for iOS >= 7
 
 @interface FLKAutoLayoutGuide : NSObject
 
 /// The actual layout guide
-@property (nonatomic, readonly, strong) id<UILayoutSupport> layoutGuide;
+@property (nonatomic, readonly, strong) NSObject *layoutGuide;
 
 /// The view that guide represents
 @property (nonatomic, readonly, strong) UIView *containerView;
 
 /// Init
-- (instancetype)initWithLayoutGuide:(id<UILayoutSupport>)layoutGuide inContainerView:(UIView *)containerView;
+- (instancetype)initWithLayoutGuide:(NSObject *)layoutGuide inContainerView:(UIView *)containerView;
 
 @end
 
@@ -19,9 +19,21 @@
 @interface UIViewController (FLKAutoLayout)
 
 /// Creates and strongly retains a topLayoutGuide for the view controller
-- (FLKAutoLayoutGuide *)flk_topLayoutGuide;
+- (FLKAutoLayoutGuide *)flk_topLayoutGuide API_DEPRECATED("No longer supported. Use flk_safeAreaLayoutGuide", ios(2.0, 10.0));
 
 /// Creates and strongly retains a bottomLayoutGuide for the view controller
-- (FLKAutoLayoutGuide *)flk_bottomLayoutGuide;
+- (FLKAutoLayoutGuide *)flk_bottomLayoutGuide API_DEPRECATED("No longer supported. Use flk_safeAreaLayoutGuide", ios(2.0, 10.0));
+
+/// Creates and strongly retains the safeAreaLayoutGuide.topAnchor for the view controller
+- (FLKAutoLayoutGuide *)flk_safeAreaTopLayoutGuide API_AVAILABLE(ios(11.0));
+
+/// Creates and strongly retains the safeAreaLayoutGuide.bottomAnchor for the view controller
+- (FLKAutoLayoutGuide *)flk_safeAreaBottomLayoutGuide API_AVAILABLE(ios(11.0));
+
+/// Creates and strongly retains the safeAreaLayoutGuide.leadingAnchor for the view controller
+- (FLKAutoLayoutGuide *)flk_safeAreaLeadingLayoutGuide API_AVAILABLE(ios(11.0));
+
+/// Creates and strongly retains the safeAreaLayoutGuide.trailingAnchor for the view controller
+- (FLKAutoLayoutGuide *)flk_safeAreaTrailingLayoutGuide  API_AVAILABLE(ios(11.0));
 
 @end
--- FLKAutoLayout/UIViewController+FLKAutoLayout.m
+++ FLKAutoLayout/UIViewController+FLKAutoLayout.m
@@ -6,7 +6,7 @@
 
 @implementation FLKAutoLayoutGuide
 
-- (instancetype)initWithLayoutGuide:(id<UILayoutSupport>)layoutGuide inContainerView:(UIView *)containerView;
+- (instancetype)initWithLayoutGuide:(NSObject *)layoutGuide inContainerView:(UIView *)containerView
 {
     if ((self = [super init])) {
         _layoutGuide = layoutGuide;
@@ -25,7 +25,7 @@ FLKAssociatedAutoLayoutGuide(UIViewController *vc, SEL type, const char *const k
 {
     FLKAutoLayoutGuide *guide = objc_getAssociatedObject(vc, key);
     if (guide == nil) {
-        NSObject<UILayoutSupport> *layoutGuide = ((id (*)(id, SEL))objc_msgSend)(vc, type);
+        NSObject *layoutGuide = ((id (*)(id, SEL))objc_msgSend)(vc, type);
         layoutGuide.flk_nameTag = [NSString stringWithFormat:@"%s.%s", class_getName(vc.class), sel_getName(type)];
         guide = [[FLKAutoLayoutGuide alloc] initWithLayoutGuide:layoutGuide inContainerView:vc.view];
         objc_setAssociatedObject(vc, key, guide, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
@@ -33,16 +33,55 @@ FLKAssociatedAutoLayoutGuide(UIViewController *vc, SEL type, const char *const k
     return guide;
 }
 
-- (FLKAutoLayoutGuide *)flk_topLayoutGuide;
+static FLKAutoLayoutGuide *
+FLKAssociatedSafeAreaAutoLayoutGuide(UIViewController *vc, SEL type, const char *const key)
+{
+    FLKAutoLayoutGuide *guide = objc_getAssociatedObject(vc, key);
+    if (guide == nil) {
+        SEL safeAreaLayoutGuideSel = @selector(safeAreaLayoutGuide);
+        UILayoutGuide *layoutGuide = ((id (*)(id, SEL))objc_msgSend)(vc, safeAreaLayoutGuideSel);
+        NSLayoutAnchor *layoutAnchor = ((id (*)(id, SEL))objc_msgSend)(layoutGuide, type);
+        layoutAnchor.flk_nameTag = [NSString stringWithFormat:@"%s.%s.%s", class_getName(vc.class), sel_getName(safeAreaLayoutGuideSel), sel_getName(type)];
+        guide = [[FLKAutoLayoutGuide alloc] initWithLayoutGuide:layoutAnchor inContainerView:vc.view];
+        objc_setAssociatedObject(vc, key, guide, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+    }
+    return guide;
+}
+
+- (FLKAutoLayoutGuide *)flk_topLayoutGuide
 {
     static char TopLayoutGuideKey;
     return FLKAssociatedAutoLayoutGuide(self, @selector(topLayoutGuide), &TopLayoutGuideKey);
 }
 
-- (FLKAutoLayoutGuide *)flk_bottomLayoutGuide;
+- (FLKAutoLayoutGuide *)flk_bottomLayoutGuide
 {
     static char BottomLayoutGuideKey;
     return FLKAssociatedAutoLayoutGuide(self, @selector(bottomLayoutGuide), &BottomLayoutGuideKey);
+}
+
+- (FLKAutoLayoutGuide *)flk_safeAreaTopLayoutGuide
+{
+    static char SafeAreaTopLayoutGuideKey;
+    return FLKAssociatedSafeAreaAutoLayoutGuide(self, @selector(topAnchor), &SafeAreaTopLayoutGuideKey);
+}
+
+- (FLKAutoLayoutGuide *)flk_safeAreaBottomLayoutGuide
+{
+    static char SafeAreaBottomLayoutGuideKey;
+    return FLKAssociatedSafeAreaAutoLayoutGuide(self, @selector(bottomAnchor), &SafeAreaBottomLayoutGuideKey);
+}
+
+- (FLKAutoLayoutGuide *)flk_safeAreaLeadingLayoutGuide
+{
+    static char SafeAreaLeadingLayoutGuideKey;
+    return FLKAssociatedSafeAreaAutoLayoutGuide(self, @selector(leadingAnchor), &SafeAreaLeadingLayoutGuideKey);
+}
+
+- (FLKAutoLayoutGuide *)flk_safeAreaTrailingLayoutGuide
+{
+    static char SafeAreaTrailingLayoutGuideKey;
+    return FLKAssociatedSafeAreaAutoLayoutGuide(self, @selector(trailingAnchor), &SafeAreaTrailingLayoutGuideKey);
 }
 
 @end
